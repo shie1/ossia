3ddbd8933b6ee977ed712451ca8edeac45b3b895
Shie1 <sonkolyw@gmail.com>
2022-06-05 16:16:02 +0200

    Fixed typo in gtag-stuff

--- public/gtag-stuff.js
+++ public/gtag-stuff.js
@@ -2,4 +2,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
-gtag('config', 'G-6RV1HQYTBK');s
+gtag('config', 'G-6RV1HQYTBK');
