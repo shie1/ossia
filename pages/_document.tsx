13cf8475159f2eb184be85f22f32f8e3e4b5309b
Shie1 <sonkolyw@gmail.com>
2022-06-05 16:13:40 +0200

    Google Analytincs Integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ export default function Document() {
             <Head >
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3057716180157458"
                     crossOrigin="anonymous"></script>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-6RV1HQYTBK"></script>
+                <script async src='/gtag-stuff.js' />
             </Head>
             <body>
                 <Main />
