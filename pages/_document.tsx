b736a3c5e48b33324e16347dfb1fb48a209e50cd
Shie1 <sonkolyw@gmail.com>
2022-06-05 17:11:28 +0200

    Update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,9 @@ export default function Document() {
     return (
         <Html>
             <Head >
-                <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3057716180157458" crossOrigin='anonymous' strategy='afterInteractive' />
-                <Script src="https://www.googletagmanager.com/gtag/js?id=G-6RV1HQYTBK" strategy='afterInteractive' />
-                <Script id="google-analytics" strategy="afterInteractive">
+                <script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3057716180157458" crossOrigin='anonymous' async/>
+                <script src="https://www.googletagmanager.com/gtag/js?id=G-6RV1HQYTBK" async/>
+                <script async>
                     {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){window.dataLayer.push(arguments);}
@@ -15,7 +15,7 @@ export default function Document() {
 
                     gtag('config', 'GA_MEASUREMENT_ID');
                     `}
-                </Script>
+                </script>
                 <script id='afly'>
                     {`
                     var adfly_id = 23309985;
