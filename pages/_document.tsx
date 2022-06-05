3ce248bc10211c6433545e238505fdd0bf4df44c
Shie1 <sonkolyw@gmail.com>
2022-06-05 15:29:26 +0200

    Google Adsense integration

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head >
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3057716180157458"
+                    crossOrigin="anonymous"></script>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
