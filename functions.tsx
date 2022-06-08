0d3294902510f3d4d5a3e02d00df9966594c791b
shie1 <sonkolybence@outlook.hu>
2022-06-08 13:22:00 +0200

    update functions.tsx

--- functions.tsx
+++ functions.tsx
@@ -1,7 +1,4 @@
-export function MetaTags(props: any) : any => {
-    const title = props.title
-    const description = props.description
-    const image = props.image
+const MetaTags => ({title,description,image}) => {
     const Image = () => {
         if(!image){return <div />}
         return (
