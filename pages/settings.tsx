bf6405c44b30f5f4f61fa320f23791079bfb15be
Shie1 <sonkolyw@gmail.com>
2022-06-14 09:08:56 +0200

    import export

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,12 +1,17 @@
-import { Accordion, AccordionItem, Text, Button, SegmentedControl, JsonInput } from '@mantine/core'
+import { Accordion, AccordionItem, Text, Button, SegmentedControl, JsonInput, Modal, TextInput, Textarea, Breadcrumbs } from '@mantine/core'
 import { useLocalStorage } from '@mantine/hooks';
 import { useModals } from '@mantine/modals';
 import { showNotification } from '@mantine/notifications';
 import type { NextPage } from 'next'
 import { Adjustments, AntennaBars5, Braces, CalendarTime, Database, DatabaseExport, DatabaseImport, DatabaseOff, X } from 'tabler-icons-react';
+import lzstring from 'lz-string'
+import { useState } from 'react';
 
 const Settings: NextPage = () => {
     const modals = useModals();
+    const [exportModal, setExportModal] = useState(false)
+    const [importModal, setImportModal] = useState(false)
+    const [exportVal, setExportVal] = useState('')
     const [lqmode, setLQMode] = useLocalStorage({
         'key': 'lowQualityMode',
         'defaultValue': 1
@@ -18,9 +23,36 @@ const Settings: NextPage = () => {
         })
     }
 
-    const importLocalStorage = comingSoon
+    const userData = ["history", "liked-songs", "lowQualityMode", "loop"]
 
-    const exportLocalStorage = comingSoon
+    const importLocalStorage = () => {
+        setImportModal(true)
+    }
+
+    const ilsModalAction = () => {
+        const input = document?.querySelector("#importInput")! as HTMLInputElement
+        if(!input.value){return}
+        const json = JSON.parse(lzstring.decompressFromBase64(input.value)!)
+        for(const item in json){
+            localStorage.setItem(item,json[item])
+        }
+        showNotification({
+            'title': "Successful import",
+            'icon': <DatabaseImport />,
+            'message': '',
+        })
+        setImportModal(false)
+    }
+
+    const exportLocalStorage = () => {
+        if (exportVal) { setExportModal(true); return }
+        let exported: any = {}
+        for (const item of userData) {
+            exported[item] = localStorage.getItem(item)
+        }
+        setExportVal(lzstring.compressToBase64(JSON.stringify(exported)))
+        setExportModal(true)
+    }
 
     const confirm = (callback: any) => {
         modals.openConfirmModal({
@@ -39,7 +71,9 @@ const Settings: NextPage = () => {
     const clearLocalStorage = () => {
         const action = () => {
             if (typeof window !== 'undefined') {
-                localStorage.clear()
+                for (const item of userData) {
+                    localStorage.removeItem(item)
+                }
                 showNotification({
                     title: 'User data cleared',
                     message: '',
@@ -74,19 +108,38 @@ const Settings: NextPage = () => {
 
     return (
         <>
+            <Modal
+                opened={exportModal}
+                onClose={() => { setExportModal(false) }}
+                title="Export code"
+            >
+                <Text mb='sm'>Paste this code to the other device&apos;s import code field.</Text>
+                <Textarea maxRows={5} onClick={(e: any) => {
+                    e.target.select()
+                }} autosize value={exportVal} />
+            </Modal>
+            <Modal
+                opened={importModal}
+                onClose={() => { setImportModal(false) }}
+                title="Import code"
+            >
+                <Text mb='sm'>Enter the code you generated on the previous device.</Text>
+                <Textarea mb='sm' onSubmit={ilsModalAction} id='importInput' />
+                <Button onClick={ilsModalAction}>Submit</Button>
+            </Modal>
             <Accordion>
                 <AccordionItem icon={<Database />} label="User Data">
                     <Accordion>
                         <AccordionItem icon={<DatabaseOff />} label="Clear User Data">
-                            <Text mb='sm'>Ossia stores liked songs, recents etc. in the browsers local storage. If you wish to clear this data, press the button.</Text>
+                            <Text mb='sm'>Ossia stores liked songs, recents etc. in the browser&apos;s local storage. If you wish to clear this data, press the button.</Text>
                             <Button onClick={clearLocalStorage}>Clear data</Button>
                         </AccordionItem>
                         <AccordionItem icon={<DatabaseImport />} label="Import User Data">
-                            <Text mb='sm'>Ossia stores liked songs, recents etc. in the browsers local storage. If you wish to import this data from another device, press the button.</Text>
+                            <Text mb='sm'>Ossia stores liked songs, recents etc. in the browser&apos;s local storage. If you wish to import this data from another device, press the button.</Text>
                             <Button onClick={importLocalStorage}>Import data</Button>
                         </AccordionItem>
                         <AccordionItem icon={<DatabaseExport />} label="Export User Data">
-                            <Text mb='sm'>Ossia stores liked songs, recents etc. in the browsers local storage. If you wish to export this data to another device, press the button.</Text>
+                            <Text mb='sm'>Ossia stores liked songs, recents etc. in the browser&apos;s local storage. If you wish to export this data to another device, press the button.</Text>
                             <Button onClick={exportLocalStorage}>Export data</Button>
                         </AccordionItem>
                     </Accordion>
