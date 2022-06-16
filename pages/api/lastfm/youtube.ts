0966cdf657ce04d38f2cd51228005713784e9e09
Shie1 <sonkolyw@gmail.com>
2022-06-16 17:45:57 +0200

    removed a fkn line

--- pages/api/lastfm/youtube.ts
+++ pages/api/lastfm/youtube.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import * as yt from 'youtube-search-without-api-key';
-import ytdl from 'ytdl-core';
 
 export default async function handler(
     req: NextApiRequest,
