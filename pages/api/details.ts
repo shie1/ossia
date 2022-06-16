1ec4c41fe53d1430af27b7ac1a6c8b6ef96537e9
Shie1 <sonkolyw@gmail.com>
2022-06-16 15:57:26 +0200

    Update

--- pages/api/details.ts
+++ pages/api/details.ts
@@ -1,11 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import ytdl from 'ytdl-core'
-import type { videoInfo } from 'ytdl-core'
+import manifest from '../../gen-manifest.mjs'
 
 export default async function handler(
     req: any,
-    res: NextApiResponse<videoInfo>
+    res: NextApiResponse<object>
 ) {
-    res.status(200).json(await ytdl.getInfo(req.query['v']))
+    res.status(200).json(manifest)
 }
