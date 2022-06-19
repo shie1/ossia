626a52d25c083c30fc3937eea1ad8cdfdde2b359
Shie1 <sonkolyw@gmail.com>
2022-06-19 19:42:59 +0200

    p

--- pages/song.tsx
+++ pages/song.tsx
@@ -28,7 +28,6 @@ const Listen: NextPage = (props: any) => {
             'title': `${songDetails.title} | Ossia`,
             'description': `Listen to ${songDetails.title} on the Ossia Music Player!`,
             'meta': {
-                'og:image': songDetails.thumbnails[songDetails.thumbnails.length - 1].url,
             }
         }} />
     </>)
