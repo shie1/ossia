84ceeb5b09cb0d9b89c2f921b5ec95b92c234656
shie1 <sonkolybence@outlook.hu>
2022-07-06 01:43:59 +0200

    Update v3.tsx

--- pages/v3.tsx
+++ pages/v3.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const V3: NextPage = () => {
-    return <meta httpEquiv="refresh" content="0;URL=https://ossia-3w4gjqvxg-shie1.vercel.app" />
+    return <meta httpEquiv="refresh" content="0;URL=https://ossia-ggj13bo8x-shie1.vercel.app" />
 }
 
 export default V3
