62671c755cc7da2b1c8ec3b8e0ffcdc7f8cae19f
Shie1 <sonkolyw@gmail.com>
2022-07-06 22:34:58 +0200

    recognize

--- pages/api/youtube/recognize.tsx
+++ pages/api/youtube/recognize.tsx
@@ -9,37 +9,39 @@ export default async function handler(
     const info: any = (await ytdl.getInfo(req.query['v'] as any))
     if (typeof info.response.engagementPanels[1].engagementPanelSectionListRenderer.content.structuredDescriptionContentRenderer.items[1] === 'undefined') { res.status(200).json(false) }
     const musicSection = info.response.engagementPanels[1].engagementPanelSectionListRenderer.content.structuredDescriptionContentRenderer.items[1].videoDescriptionMusicSectionRenderer
-    for (const lockup of musicSection.carouselLockups) {
-        let innerResp: any = {}
-        if (typeof lockup.carouselLockupRenderer.infoRows !== 'undefined') {
-            for (const row of lockup.carouselLockupRenderer.infoRows) {
-                const renderer = row.infoRowRenderer
-                const title = renderer.title.simpleText
-                let input: any = typeof renderer.defaultMetadata !== 'undefined' ? renderer.defaultMetadata : renderer.expandedMetadata
-                switch (title) {
-                    case "SONG":
-                        input = input.simpleText
-                        break
-                    case "ARTIST":
-                        input = typeof input.simpleText === 'undefined' ? input.runs[0].text : input.simpleText
-                        break
-                    case "LICENSES":
-                        input = input.simpleText
-                        break
-                    case "ALBUM":
-                        input = input.simpleText
-                        break
-                    case "WRITERS":
-                        input = input.simpleText
-                        break
-                    default:
-                        input = ""
-                        break
-                }
-                if (input) { innerResp[title] = input }
-            }
-            resp.push(innerResp)
+    const lockups = musicSection.carouselLockups
+    for (const lockup of lockups) {
+        let inner: any = {}
+        const renderer = lockup.carouselLockupRenderer
+        if (typeof renderer.videoLockup !== 'undefined') {
+            inner["SONG"] = renderer.videoLockup.compactVideoRenderer.title.runs[0].text
         }
+        for (const infoRow of renderer.infoRows) {
+            const title = infoRow.infoRowRenderer.title.simpleText
+            let input: any = typeof infoRow.infoRowRenderer.defaultMetadata !== 'undefined' ? infoRow.infoRowRenderer.defaultMetadata : infoRow.infoRowRenderer.expandedMetadata
+            switch (title) {
+                case "SONG":
+                    input = input.simpleText
+                    break
+                case "ARTIST":
+                    input = typeof input.simpleText === 'undefined' ? input.runs[0].text : input.simpleText
+                    break
+                case "LICENSES":
+                    input = input.simpleText
+                    break
+                case "ALBUM":
+                    input = input.simpleText
+                    break
+                case "WRITERS":
+                    input = input.simpleText
+                    break
+                default:
+                    input = ""
+                    break
+            }
+            if (input) { inner[title] = input }
+        }
+        resp.push(inner)
     }
     res.status(200).json(resp)
 }
