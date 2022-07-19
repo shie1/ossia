1034e25be4f04c1d184109d2214904a185bdc7d3
Shie1 <sonkolyw@gmail.com>
2022-07-19 20:29:16 +0200

    fixed preview build

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,7 +10,7 @@ const Page404: NextPage = () => {
         </Head>
         <Group position="center" align="center" sx={{ height: '95%' }} direction="row">
             <Center mx={30}>
-                <Image sx={{ maxWidth: '38vh' }} src="https://place-hold.it/720x1080&fontsize=50" />
+                <Image alt="Sad robot on tower" sx={{ maxWidth: '38vh' }} src="https://place-hold.it/720x1080&fontsize=50" />
             </Center>
             <Group mx={30} direction="column" spacing={4}>
                 <Title sx={{ fontSize: '3rem' }}>404</Title>
