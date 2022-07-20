027213817db62bf1e23534ef87f6eb83b35940b4
Shie1 <sonkolyw@gmail.com>
2022-07-20 19:22:46 +0200

    Update

--- pages/api/user/authenticated.ts
+++ pages/api/user/authenticated.ts
@@ -8,18 +8,20 @@ export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ) {
-    const token = getCookie("token", { req: req, res: res, path: "/", httpOnly: true })
-    if (!token) { return res.status(200).json(false) }
-    try {
-        const jwt = verify(token as string, process.env["OSSIA_PRIVATE_KEY"] as string) as any
-        executeQuery("SELECT * FROM `users` WHERE (`username` = ? and `salt` = ?)", [jwt.username, jwt.salt]).then(resp => {
-            if (resp[0]) {
-                res.status(200).json(jwt)
-            } else {
-                res.status(200).json(false)
-            }
-        })
-    } catch {
-        res.status(200).json(false)
-    }
+    return new Promise((resolve, reject) => {
+        const token = getCookie("token", { req: req, res: res, path: "/", httpOnly: true })
+        if (!token) { return resolve(res.status(200).json(false)) }
+        try {
+            const jwt = verify(token as string, process.env["OSSIA_PRIVATE_KEY"] as string) as any
+            executeQuery("SELECT * FROM `users` WHERE (`username` = ? and `salt` = ?)", [jwt.username, jwt.salt]).then(resp => {
+                if (resp[0]) {
+                    return resolve(res.status(200).json(jwt))
+                } else {
+                    return resolve(res.status(200).json(false))
+                }
+            })
+        } catch {
+            return resolve(res.status(200).json(false))
+        }
+    })
 }
