d503c9327fc448111a0c15422547078466ce6639
Shie1 <sonkolyw@gmail.com>
2022-07-21 05:47:27 +0200

    Update

--- pages/api/user/login.tsx
+++ pages/api/user/login.tsx
@@ -10,13 +10,13 @@ export default function handler(
     res: NextApiResponse<any>
 ) {
     return new Promise((resolve, reject) => {
-        let rb = JSON.parse(req.body)
-
-        executeQuery("SELECT `salt` FROM `users` WHERE (`username` = ?);", [rb.username]).then(salt => {
+        let { username, password } = JSON.parse(req.body)
+        executeQuery("SELECT `salt` FROM `users` WHERE (`username` = ?);", [username]).then(salt => {
+            console.log(salt)
             if (!salt[0]) { return resolve(res.status(200).json(false)) }
             salt = salt[0].salt
-            const hash = pbkdf2Sync(rb.password, salt, 1000, 64, `sha512`).toString(`hex`);
-            executeQuery("SELECT * FROM `users` WHERE (`username` = ? and `password` = ? and `salt` = ?);", [rb.username, hash, salt]).then(resp => {
+            const hash = pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`);
+            executeQuery("SELECT * FROM `users` WHERE (`username` = ? and `password` = ? and `salt` = ?);", [username, hash, salt]).then(resp => {
                 resp = resp[0]
                 if (typeof resp !== "undefined") {
                     const token = sign({ username: resp.username, salt: resp.salt }, process.env["OSSIA_PRIVATE_KEY"] as string)
