01b275419394618a49e00b550c9a6fce2050f16d
Shie1 <sonkolyw@gmail.com>
2022-07-24 18:25:47 +0200

    Updated domain name for sitemap

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-    siteUrl: process.env.SITE_URL || 'https://ossia.ml',
+    siteUrl: process.env.SITE_URL || 'https://ossia.rocks',
     generateRobotsTxt: true,
 }
 
