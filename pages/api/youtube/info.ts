4fd505e2b765774e9810d5b9c7e568cef42a050d
Shie1 <sonkolyw@gmail.com>
2022-08-09 13:55:24 +0200

    Removed api/youtube/info

--- pages/api/youtube/info.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { getInfo } from "ytdl-core"
-
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<any>
-) {
-    res.status(200).json(await (getInfo(req.query['v'])))
-}
